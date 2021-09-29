18e98354172e30bb25fb43828c04f7c608debf80
okpalaChidiebere <okpalacollins4@gmail.com>
2021-09-28 20:02:52 -0600

    first commit

---
+++ mobile/App.js
@@ -0,0 +1,89 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import io from "socket.io-client"; //must use for react native
+
+const socketUrl = "http://127.0.0.1:3000"; //Run ifconfig on Mac or ipconfig on windows to get the ipAddress for your wifi
+export default function App() {
+  const socket = React.useRef(null);
+  const [state, setState] = React.useState({
+    isConnected: false, //ideally we will store this in our redux store :)
+    chatMessage: "",
+    chatMessages: [],
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      socket.current = openSocket();
+
+      socket.current.on("connect", () => {
+        setState((currState) => ({
+          ...currState,
+          isConnected: true,
+        }));
+      });
+
+      /** Here we receive messages from our server through this socket connection*/
+      socket.current.on("ping", (msg) => {
+        setState((currState) => ({
+          ...currState,
+          chatMessages: [...currState.chatMessages, msg],
+        }));
+      });
+    })();
+  }, []);
+
+  const submitChatMessage = () => {
+    //NOTE: that we use the 'emit' method to send messages
+    socket.current.emit("chat_message", state.chatMessage);
+    setState((currState) => ({
+      ...currState,
+      chatMessage: "",
+    }));
+  };
+
+  const { chatMessage, chatMessages } = state;
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <TextInput
+        style={{ height: 40, borderWidth: 2 }}
+        autoCorrect={false}
+        value={chatMessage}
+        onSubmitEditing={() => submitChatMessage()} //when we press the enter key, then we run the submitChatMessage method
+        onChangeText={(chatMessage) => {
+          setState((currState) => ({
+            ...currState,
+            chatMessage,
+          }));
+        }}
+      />
+      {chatMessages.map((chatMessage) => (
+        <Text
+          key={
+            chatMessage /**ideally your will have your uniqueId for the chat message here :) */
+          }
+        >
+          {chatMessage}
+        </Text>
+      ))}
+    </View>
+  );
+}
+
+function openSocket() {
+  const conn = io(socketUrl, {
+    transports: ["websocket"],
+  });
+  return conn;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    //alignItems: "center",
+    justifyContent: "center",
+  },
+});
